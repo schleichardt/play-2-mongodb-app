05e804b44acf9e7d0dee65e2439cbaf4b7441d55
Michael Schleichardt <michael@schleichardt.net>
2012-10-13 17:32:26 +0200

    added dummy test for jenkins

--- test/PlaceholderSpec.scala
+++ test/PlaceholderSpec.scala
@@ -1,7 +1,7 @@
 import org.specs2.mutable._
 
 
-class DbConnectionSpec extends Specification {
+class PlaceholderSpec extends Specification {
   "nothing to test" can {
     "work" in {
       1 === 2 - 1

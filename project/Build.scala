6a2e37576dfcc1167e7eceb5ca7c87eb6ba88574
Michael Schleichardt <michael@schleichardt.net>
2012-10-13 10:23:15 +0200

    moved MongoDB tests to integration test

--- project/Build.scala
+++ project/Build.scala
@@ -4,16 +4,22 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "play-2-mongodb-app"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName = "play-2-mongodb-app"
+  val appVersion = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      "se.radley" %% "play-plugins-salat" % "1.1"
-    )
+  val appDependencies = Seq(
+    "se.radley" %% "play-plugins-salat" % "1.1"
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      routesImport += "se.radley.plugin.salat.Binders._",
-      templatesImport += "org.bson.types.ObjectId"
-    )
-
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    routesImport += "se.radley.plugin.salat.Binders._",
+    templatesImport += "org.bson.types.ObjectId"
+  ).configs(IntegrationTest)
+    .settings(Defaults.itSettings: _*)
+    .settings(
+    libraryDependencies += "play" %% "play-test" % play.core.PlayVersion.current % "it"
+    , scalaSource in IntegrationTest <<= baseDirectory / "it"
+    , javaSource in IntegrationTest <<= baseDirectory / "it"
+    , sourceDirectory in IntegrationTest <<= baseDirectory / "it"
+  )
 }

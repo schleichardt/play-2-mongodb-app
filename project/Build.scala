c2b266ccb6675b4ca32b0aed5c71b8bb25a4bdb0
Michael Schleichardt <michael@schleichardt.net>
2013-02-28 15:48:42 +0100

    added embedmongodependency

--- project/Build.scala
+++ project/Build.scala
@@ -10,6 +10,7 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     "org.reactivemongo" %% "reactivemongo" % "0.9-SNAPSHOT"
     , "junit" % "junit-dep" % "4.11"
+    , "de.flapdoodle.embed" % "de.flapdoodle.embed.mongo" % "1.28"
   )
 
 

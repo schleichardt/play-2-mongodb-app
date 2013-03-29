e3ee857f679d8c69dbee0b9eff4aadc625edb897
Michael Schleichardt <michael@schleichardt.net>
2013-03-29 19:57:49 +0100

    reserve port for Embedded MongoDB, parallel running tests should not try to start a MongoDB with the same port at the same time

--- app/Global.scala
+++ app/Global.scala
@@ -1,5 +1,5 @@
 import play.api.{Logger, Application, Configuration, GlobalSettings}
-import plugins.embedmongo.EmbedMongoPlugin.freePort
+import plugins.embedmongo.EmbedMongoPlugin.reservePort
 import scala.collection.JavaConverters._
 
 object Global extends GlobalSettings{
@@ -12,7 +12,7 @@ object Global extends GlobalSettings{
   override def configuration = {
     val embedmongoActive = super.configuration.getBoolean("embedmongo.enabled").getOrElse(true)
     if (embedmongoActive) {
-      val port = freePort()
+      val port = reservePort()
       super.configuration ++
         Configuration.from(Map("embedmongo.port" -> port, "mongodb.servers" -> List(s"localhost:$port").asJava))
     } else super.configuration
